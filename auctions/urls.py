0bd0cc558245af7d182319c42a829226e707d7c7
AbdelRahman Tamer <abdulrahmantamer765@gmail.com>
2026-02-16 17:22:00 +0200

    Applying urls best practices (appending / to the end and separating each group)

--- auctions/urls.py
+++ auctions/urls.py
@@ -4,14 +4,18 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("login", views.login_view, name="login"),
-    path("logout", views.logout_view, name="logout"),
-    path("register", views.register, name="register"),
+
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),
+    path("register/", views.register, name="register"),
+
+    path("listing/<int:listing_id>/", views.listing, name="listing"),
     path("listing/<int:listing_id>/like/", views.toggle_like, name="toggle_like"),
-    path("listing/<int:listing_id>", views.listing, name="listing"),
-    path("listing/<int:listing_id>/comment", views.comment, name="comment"),
-    path("watchlist_toggle", views.watchlist_toggle, name="watchlist_toggle"),
-    path("watchlist", views.watchlist_view, name="watchlist_view"),
-    path("category", views.categories, name="categories"),
-    path("listing/<int:listing_id>/cart/", views.toggle_cart, name="toggle_cart")
+    path("listing/<int:listing_id>/comment/", views.comment, name="comment"),
+    path("listing/<int:listing_id>/cart/", views.toggle_cart, name="toggle_cart"),
+
+    path("watchlist/", views.watchlist_view, name="watchlist_view"),
+    path("watchlist_toggle/", views.watchlist_toggle, name="watchlist_toggle"),
+    path("category/", views.categories, name="categories"),
 ]
+
